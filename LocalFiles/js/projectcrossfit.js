a46a80d0bea37470ae99fabcd340ca2ec7438b52
Esben Erland <esben.erland@gmail.com>
2013-05-25 11:38:05 +0200

    Equipment page including new icons and html5 localstorage to store selections

--- LocalFiles/js/projectcrossfit.js
+++ LocalFiles/js/projectcrossfit.js
@@ -22,15 +22,43 @@ function loadEquipmentHtmlFromJson() {
 			// Tell jQuery Mobile to enhance the inserted html as it was
 			// inserted by an AJAX call
 			$('#equipmentList').listview('refresh')
+			
+			
+			//Bind to "click" on the list items
+			$(".equipment-clickable").on("touchend mousedown", function(event) {
+				var span = $(this).children('span');
+				span.toggle();
+				
+				var visible = span.is(":visible");
+				
+				//Store in localstorage
+				if(typeof(Storage)!=="undefined") {
+					var key = "equipment" + $(this).attr("data-equipmentId");
+					localStorage.setItem(key, visible);
+				}				  
+			});
+			
+			
 		}
 	});
 }
 
 function createEquipmentHtml(equipmentItem) {
-	var html = '<li style="text-transform: capitalize">' + '<img src="data/'
-			+ equipmentItem.Icon
-			+ '" onerror="this.src=\'data/doesnotexist.png\';">'
-			+ equipmentItem.Name + '</li>';
+	var key = "equipment" + equipmentItem.Id;
+	var val = localStorage.getItem(key);
+	var displayStyle = "";
+	
+	if(val != "undefined" && val == "false")
+		 displayStyle = ' style="display:none"';
+	
+	var html =  		
+	'<li data-icon="false" class="ui-li-custom-equipment">' +
+	'<a href="#" class="equipment-clickable" data-equipmentId=' + equipmentItem.Id  + '>' +
+	'<img src="data/' + equipmentItem.Icon + '">' + 
+	'<span class="checkmark-image-overlay"' + displayStyle + '></span>' +
+	equipmentItem.Name +
+	'</a>';
+	
 	return html;
 }
 
