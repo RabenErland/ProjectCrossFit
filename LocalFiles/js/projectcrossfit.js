d3a4dec99b777359e82d764d0ee932e6a5c9e278
Esben Erland <esben.erland@gmail.com>
2013-05-26 15:48:50 +0200

    wodlist page

--- LocalFiles/js/projectcrossfit.js
+++ LocalFiles/js/projectcrossfit.js
@@ -51,16 +51,50 @@ Wod.getStorageKey = function(wodId) {
 	return "projectCrossFit_Wod" + wodId;
 }
 
+Wod.prototype.getStorageKey = function() {
+	return Wod.getStorageKey(this.wodJson.Id);
+}
+
 Wod.prototype.createListHtml = function() {
 	//Determine if the equipment item should be shown
 	var exercises = this.wodJson.Exercises;
-	exercises.replace('[', '');
-	exercises.replace(']', '');
+	var bracketRegex = /(\[.+?\])|(:|\.)/g;  //locate all square brackets and colons
 	
+	var exerciseText = '<p class="ui-li-desc"><i>'; //The formatted html for the exercises
+	var lastNum = 0; //The last index in the json text that was handled
+	var newParagraph = false; //Whether any new <p> elements were introduced
+	
+	while (m = bracketRegex.exec(exercises)) {		
+		//add text prior to match (or in between matches)
+		exerciseText += exercises.substring(lastNum, m.index);
+		var matchText = m[0];
+
+		if(matchText == ":" || matchText == '.') {
+			//Add paragraph on : or .
+			
+			//End existing paragraph if any was introduced
+			if(newParagraph)
+				exerciseText += '</p>';
+			
+			exerciseText += newParagraph ? '<p>' : '</i><p>';
+			newParagraph = true;
+			
+		} 
+		else {
+			//get exercise text without brackets and add tags to highlight exercise names
+			exerciseText += '<strong>' + matchText.substring(1, matchText.length-1) + '</strong>';
+		}
+		
+		lastNum = m.index + matchText.length;
+	} 
+	
+	//End initial paragraph
+	exerciseText += '</p>';
+
 	var html =  		
-		'<li><a href="woddetail?'+ this.wodJson.Id + '">' +						
+		'<li><a href="woddetail.html?id=' + this.wodJson.Id + '"">' +						
 		'<h2>' + this.wodJson.Name + '</h2>' +
-		'<p class="ui-li-desc">' + exercises + '</p>' +		
+		'<p class="ui-li-desc">' + exerciseText +		
 		'</a></li>'	
 	return html;
 }
@@ -78,13 +112,19 @@ function loadWodHtmlFromJson() {
 		//Determine if html element exists for injection
 		if ($("#wodList").length > 0) { 
 
+			var wodLookup = {};
+			
 			var html = "";
 			// Iterate through list and create html
 			$.each(wodList, function(i, wodItem) {
-				var eq = new Wod(wodItem);
-				html += eq.createListHtml();
+				var wod = new Wod(wodItem);
+				html += wod.createListHtml();
+				
+				wodLookup["1"] = wod;
 			});
 
+			window.wodLookup = wodLookup;
+			
 			$(html).appendTo("#wodList");
 
 			// Tell jQuery Mobile to enhance the inserted html as it was
@@ -161,4 +201,22 @@ function toggleMenu() {
 	else {
 		menupanel.panel("close");
 	}
+}
+
+function getIdFromQueryParam() {
+	var query = $(this).data("url");
+	if(query != undefined) {
+		query = query.split("?");
+		if(query.length < 1)
+			return undefined;
+						
+		query = query[1].replace("id=","");
+	}
+	else {
+		//Handle situations where JQM is not in control (e.q. reload in browser)
+		var query = window.location.search;
+		query = query.replace("?id=","");
+	}
+		
+	return query;
 }
